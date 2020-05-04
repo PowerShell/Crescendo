b9b19ece02b9003321d162fea430dd814ab7ef78
Andrew Menagarishvili <anmenaga@ntdev.microsoft.com>
2020-05-04 14:54:34 -0700

    Added spaces in EOFs

--- Microsoft.PowerShell.NativeCommandProxy/src/KubectlGenerator.psm1
+++ Microsoft.PowerShell.NativeCommandProxy/src/KubectlGenerator.psm1
@@ -5,6 +5,6 @@ function New-KubectlProxy
         # if non-empty - save generated module to this path
         [Parameter(Mandatory=$False)][string] $SaveToPath
     )
-    
+
     "Hello from New-KubectlProxy"
 }
