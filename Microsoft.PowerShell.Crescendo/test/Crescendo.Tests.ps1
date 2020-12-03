b481467e223ef28ca0c1cad4483cbb3e070a0b5d
James Truher <jimtru@microsoft.com>
2020-12-03 10:46:23 -0800

    change some test code to attempt to narrow the problem

--- Microsoft.PowerShell.Crescendo/test/Crescendo.Tests.ps1
+++ Microsoft.PowerShell.Crescendo/test/Crescendo.Tests.ps1
@@ -1,18 +1,18 @@
 Describe "Unit tests for Microsoft.PowerShell.Crescendo" -tags CI {
 
     BeforeAll {
-        $moduleName = "Microsoft.PowerShell.Crescendo"
-        $moduleManifest = "${moduleName}.psd1"
+        #$moduleName = "Microsoft.PowerShell.Crescendo"
+        #$moduleManifest = "${moduleName}.psd1"
         $noun = "Crescendo"
         $verb = 'Get'
-        $modulePath = [System.Io.Path]::combine($PSScriptRoot,"..","src","Microsoft.PowerShell.Crescendo.psd1")
-        $moduleManifestPath = (Resolve-Path $modulePath).Path
-        import-module $moduleManifestPath
+        #$modulePath = [System.Io.Path]::combine($PSScriptRoot,"..","src","Microsoft.PowerShell.Crescendo.psd1")
+        #$moduleManifestPath = (Resolve-Path $modulePath).Path
+        #import-module $moduleManifestPath
     }
 
-    AfterAll {
-        Remove-Module $moduleName
-    }
+    #AfterAll {
+    #    Remove-Module $moduleName
+    #}
 
     It "is possible to create a command object" {
         $pc = New-CrescendoCommand -Verb $verb -Noun $noun
@@ -34,7 +34,7 @@ Describe "Unit tests for Microsoft.PowerShell.Crescendo" -tags CI {
             $pc.Description = "this is a description"
             $pc.Usage = "this is usage" # this allows for creating a synopsis because of the default constructor for usage
             $s = $pc.ToString()
-            $expectedResult = (Get-Content "${PSScriptRoot}/assets/ProxyContentTest1.txt") -join "`n"
+            $expectedResult = (Get-Content "${PSScriptRoot}/assets/ProxyContentTest1.txt") -join [environment]::newline
             $s | Should -Be $expectedResult
         }
 
@@ -45,7 +45,7 @@ Describe "Unit tests for Microsoft.PowerShell.Crescendo" -tags CI {
             $pc.Usage = "this is usage" # this allows for creating a synopsis because of the default constructor for usage
             $pc.Parameters.Add((New-ParameterInfo -Name "pName" -OriginalName "--OriginalName"))
             $s = $pc.ToString()
-            $expectedResult = (Get-Content "$PSScriptRoot/assets/ProxyContentTest2.txt") -join "`n"
+            $expectedResult = (Get-Content "$PSScriptRoot/assets/ProxyContentTest2.txt") -join [environment]::newline
             $s | Should -Be $expectedResult
         }
     }
