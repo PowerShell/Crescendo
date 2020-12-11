a5984e8cf699170c038acc772b2eef63122ebe23
James Truher <jimtru@microsoft.com>
2020-12-11 12:15:16 -0800

    fix logic issue with script block for testing PS5

--- Microsoft.PowerShell.Crescendo/test/ProxyViaSchema.Tests.ps1
+++ Microsoft.PowerShell.Crescendo/test/ProxyViaSchema.Tests.ps1
@@ -53,7 +53,7 @@ Describe "Create proxy from Schema" -tags CI {
         It "PowerShell 5 can parse the module" -skip:$(!$IsWindows) {
             $result = powershell.exe -c '
             $t = $e = $null
-            $ast = [System.Management.Automation.Language.Parser]::ParseFile(''$modulePath'',[ref]$t,[ref]$e)
+            $ast = [System.Management.Automation.Language.Parser]::ParseFile(''' + $modulePath + ''',[ref]$t,[ref]$e)
             $e.Count'
             $result | Should -Be 0
         }
