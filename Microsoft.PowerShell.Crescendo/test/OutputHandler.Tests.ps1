d18840591bd870415bb968bd7002d98cbf623503
James Truher <jimtru@microsoft.com>
2021-07-14 17:02:34 -0700

    Support Inline, Function, and Script output handlers.
    Be sure to pass DefaultMissingValue if provided.
    Minor fixes to schema.

---
+++ Microsoft.PowerShell.Crescendo/test/OutputHandler.Tests.ps1
@@ -0,0 +1,41 @@
+Describe "Different types of output handlers are supported" {
+	BeforeAll {
+		$savedPath = $env:PATH
+		$env:PATH = "$TESTDRIVE" + [System.IO.Path]::PathSeparator + $env:PATH
+		# we have to put this in the global namespace, otherwise it will not be found
+		function global:Convert-GetDate {
+			[CmdletBinding()]
+			param ([Parameter(ValueFromPipeline=$true,Position=0,Mandatory=$true)][DateTime]$date)
+			PROCESS { "function:" + $date.date.ToString("MM/dd/yyyy") }
+		}
+
+'
+[CmdletBinding()]
+param ([Parameter(ValueFromPipeline=$true,Position=0,Mandatory=$true)][DateTime]$date)
+PROCESS { "script:" + $date.date.ToString("MM/dd/yyyy") }
+' > "$TESTDRIVE/convert-getdate.ps1"
+			New-Item -ItemType Directory $TESTDRIVE/export
+			Export-CrescendoModule -ModuleName $TESTDRIVE/export/multimodule -ConfigurationFile "$PSScriptRoot/assets/MultiHandler.json"
+			Import-Module $TESTDRIVE/export/multimodule.psd1
+	}	
+	AfterAll {
+		$env:PATH = $savedPath
+		Remove-Module multimodule
+	}
+
+	It "inline output handler works correctly" {
+		$result = Invoke-GetDate -ViaInline
+		$result | Should -Match "^inline:"
+	}
+
+	It "function output handler works correctly" {
+		$result = Invoke-GetDate -Viafunction
+		$result | Should -Match "^function:"
+	}
+
+	It "script output handler works correctly" {
+		$result = Invoke-GetDate -Viascript
+		$result | Should -Match "^script:"
+	}
+
+}
