e5368c6fa3a5001f32e926e1fe4367be0028d362
James Truher [MSFT] <jimtru@microsoft.com>
2022-11-29 14:08:02 -0800

    Add tests for push/pop CrescendoError (#184)
    * Add tests for Pop/Push CrescendoError functions.
    
    Add new schema for new file in packaging tests.
    
    * Revert "Add tests for Pop/Push CrescendoError functions."
    
    This reverts commit 265347fbc213e2a246ae34d058f0b1200d6f64eb.
    
    * Add tests for Pop/Push CrescendoError functions.
    
    Add new schema for new file in packaging tests.
    
    * Don't run tests which rely on /bin/ls on Windows.

---
+++ Microsoft.PowerShell.Crescendo/test/PopCrescendoError.Tests.ps1
@@ -0,0 +1,40 @@
+Describe "Pop-CrescendoError tests" {
+    BeforeAll {
+        $configPath = Join-Path $PSScriptRoot "assets" "ls.poperror.proxy.json"
+        Push-Location ${TESTDRIVE}
+        Export-CrescendoModule -ConfigurationFile $configPath -ModuleName "PopError"
+        Pop-Location
+        Import-Module -Pass "${TESTDRIVE}\PopError.psd1" -ErrorAction Ignore
+    }
+
+    It "Should not create an Error record if Pop-CrescendoError is not used with -EmitAsError" -skip:${IsWindows} {
+        $error.Clear()
+        $result = invoke-filelistproxy3 -Path ThisPathDoesNotExist
+        $error.Count | Should -Be 0
+        $result | Should -Match "ERROR.*ThisPathDoesNotExist"
+    }
+
+    It "Should create an Error record if Pop-CrescendoError is used with -EmitAsError" -skip:${IsWindows} {
+        $error.Clear()
+        $result = invoke-filelistproxy4 -Path ThisPathDoesNotExist -ErrorAction SilentlyContinue
+        $error.Count | Should -Be 1
+        $error[0].Exception.Message | Should -Match "ThisPathDoesNotExist"
+        $result | Should -BeNullOrEmpty
+    }
+
+    It "Should respect ErrorVariable parameter" -skip:${IsWindows} {
+        $result = invoke-filelistproxy4 -Path ThisPathDoesNotExist -ErrorVariable err -ErrorAction SilentlyContinue
+        $result | Should -BeNullOrEmpty
+        $err.Count | Should -Be 1
+        $err[0].Exception.Message | Should -Match "ThisPathDoesNotExist"
+    }
+
+    It "Should not export the pop helper function" {
+        Get-Command -Module PopError | Where-Object Name -eq "Pop-CrescendoError" | Should -BeNullOrEmpty
+    }
+
+    It "Should not export the push helper function" {
+        Get-Command -Module PopError | Where-Object Name -eq "Push" | Should -BeNullOrEmpty
+    }
+
+}
