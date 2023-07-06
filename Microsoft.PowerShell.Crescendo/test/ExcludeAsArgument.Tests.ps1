a7925562bb6db9b9d5a209ce6aeaf57d76bbfd2f
James Truher <jimtru@microsoft.com>
2023-07-05 17:07:24 -0700

    Add path to EchoTool.

--- Microsoft.PowerShell.Crescendo/test/ExcludeAsArgument.Tests.ps1
+++ Microsoft.PowerShell.Crescendo/test/ExcludeAsArgument.Tests.ps1
@@ -1,7 +1,15 @@
 Describe "Test the ExcludeAsArgument functionality" {
     BeforeAll {
+        $originalPath = $env:PATH
+        # add the current test path to the path so we can find the test executable
+        $pSeparator = [io.path]::PathSeparator
+        $env:PATH += "${pSeparator}${PSScriptRoot}"
         Export-CrescendoModule -ConfigurationFile "$PSScriptRoot/assets/ExcludeAsArg.json" -ModuleName "$TestDrive/ExcludeAsArgument"
         Import-Module "$TestDrive/ExcludeAsArgument"
+    }
+
+    AfterAll {
+        $env:PATH = $originalPath
     }
 
     It "Should support the ExcludeAsArgument parameter" {
